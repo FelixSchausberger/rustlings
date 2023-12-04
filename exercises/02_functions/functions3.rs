058b8bf9a2177c69d6749a6ca20e125f2085645a
Felix Schausberger <fel.schausberger@gmail.com>
2023-12-05 00:03:02 +0100

    22/96

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -1,4 +1,13 @@
-fn call_me(num: u8) {
+// functions3.rs
+//
+// Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    call_me(3);
+}
+
+fn call_me(num: u32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
--- exercises/03_if/if2.rs
+++ exercises/03_if/if2.rs
@@ -1,9 +1,17 @@
-// TODO: Fix the compiler error on this function.
-fn picky_eater(food: &str) -> &str {
-    if food == "strawberry" {
-        "Yummy!"
+// if2.rs
+//
+// Step 1: Make me compile!
+// Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
+//
+// Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
+
+pub fn foo_if_fizz(fizzish: &str) -> &str {
+    if fizzish == "fizz" {
+        "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
