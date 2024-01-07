0c44dcbd5a22f0998b618e7650c9003dcf51d076
Felix Schausberger <fel.schausberger@gmail.com>
2024-01-07 17:56:12 +0100

    41/96

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,22 @@
-fn trim_me(input: &str) -> &str {
-    // TODO: Remove whitespace from both ends of a string.
+// strings3.rs
+//
+// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
+// hint.
+
+fn trim_me(input: &str) -> String {
+    // TODO: Remove whitespace from both ends of a string!
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There are multiple ways to do this.
+    // TODO: Add " world!" to the string! There's multiple ways to do this!
+    let s2 = String::from(" world!");
+    input.to_owned() + &s2
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons".
-}
-
-fn main() {
-    // You can optionally experiment here.
+    // TODO: Replace "cars" in the string with "balloons"!
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
