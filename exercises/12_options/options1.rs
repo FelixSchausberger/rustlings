38311b4eb66545e219a7760dd792af254431324c
Felix Schausberger <schausberger@magazino.eu>
2024-01-17 17:25:24 +0100

    52/96

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -6,8 +6,21 @@ fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
 }
 
-fn main() {
-    // You can optionally experiment here.
+// This function returns how much icecream there is left in the fridge.
+// If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
+// all, so there'll be no more left :(
+fn maybe_icecream(time_of_day: u16) -> Option<u16> {
+    // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
+    // value of 0 The Option output should gracefully handle cases where
+    // time_of_day > 23.
+    // TODO: Complete the function body - remember to return an Option!
+    if time_of_day < 22 {
+        Some(5)
+    } else if time_of_day >= 22 && time_of_day <= 24 {
+        Some(0)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -24,13 +37,10 @@ mod tests {
     }
 
     #[test]
-    fn check_ice_cream() {
-        assert_eq!(maybe_ice_cream(0), Some(5));
-        assert_eq!(maybe_ice_cream(9), Some(5));
-        assert_eq!(maybe_ice_cream(18), Some(5));
-        assert_eq!(maybe_ice_cream(22), Some(0));
-        assert_eq!(maybe_ice_cream(23), Some(0));
-        assert_eq!(maybe_ice_cream(24), None);
-        assert_eq!(maybe_ice_cream(25), None);
+    fn raw_value() {
+        // TODO: Fix this test. How do you get at the value contained in the
+        // Option?
+        let icecreams = maybe_icecream(12).unwrap();
+        assert_eq!(icecreams, 5);
     }
 }
--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -1,15 +1,20 @@
-// TODO: This function refuses to generate text to be printed on a nametag if
-// you pass it an empty string. It'd be nicer if it explained what the problem
-// was instead of just returning `None`. Thankfully, Rust has a similar
-// construct to `Option` that can be used to express error conditions. Change
-// the function signature and body to return `Result<String, String>` instead
-// of `Option<String>`.
-fn generate_nametag_text(name: String) -> Option<String> {
+// errors1.rs
+//
+// This function refuses to generate text to be printed on a nametag if you pass
+// it an empty string. It'd be nicer if it explained what the problem was,
+// instead of just sometimes returning `None`. Thankfully, Rust has a similar
+// construct to `Option` that can be used to express error conditions. Let's use
+// it!
+//
+// Execute `rustlings hint errors1` or use the `hint` watch subcommand for a
+// hint.
+
+pub fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
-        // Empty names aren't allowed
-        None
+        // Empty names aren't allowed.
+        Err("`name` was empty; it must be nonempty.".to_string())
     } else {
-        Some(format!("Hi! My name is {name}"))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
 
